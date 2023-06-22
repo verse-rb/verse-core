4962fb4921469b7becf092ab784f26b539911ec5
Yacine <yacine.petitprez@oivan.com>
2023-06-22 13:45:23 +0700

    Fix deep symbolize keys allowing every type of value in entry

--- lib/verse/util/hash_util.rb
+++ lib/verse/util/hash_util.rb
@@ -9,19 +9,17 @@ module Verse
       #
       # @param hash [Hash] the hash to convert
       # @return [Hash] the converted hash
-      def deep_symbolize_keys(hash)
-        hash.map do |k, v|
-          v = case v
-              when Hash
-                deep_symbolize_keys(v)
-              when Array
-                v.map{ |x| deep_symbolize_keys(x) }
-              else
-                v
-              end
-
-          [k.to_sym, v]
-        end.to_h
+      def deep_symbolize_keys(value)
+        case value
+        when Array
+          value.map{ |x| deep_symbolize_keys(x) }
+        when Hash
+          value.map do |k, v|
+            [k.to_sym, deep_symbolize_keys(v)]
+          end.to_h
+        else
+          value
+        end
       end
     end
   end
