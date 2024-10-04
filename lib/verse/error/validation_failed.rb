054a43032db1cd6fad67b4ad3a44bf39330d02c0
Yacine <anykeyh@gmail.com>
2024-10-04 18:14:28 +0700

    More descriptive label for validation failed errors

--- lib/verse/error/validation_failed.rb
+++ lib/verse/error/validation_failed.rb
@@ -7,13 +7,19 @@ module Verse
     class ValidationFailed < Base
       def initialize(result = nil)
         if result.respond_to?(:errors)
-          super("verse.errors.validation_failed")
-          @source = result&.errors
+          super(map_errors(result.errors))
+          @source = result.errors
         elsif result.is_a?(String)
           super(result)
         else
           super("verse.errors.validation_failed")
         end
+      end
+
+      def map_errors(errors)
+        errors.map do |field, messages|
+          "#{field}: #{messages.join(", ")}"
+        end.flatten.join(", ")
       end
 
       http_code 422
